3677414b0d022cef5f2e6daeac2035a82edf3f56
arunlajayan <arunlajayan@gmail.com>
2023-12-13 21:19:21 +0530

    authentication componenet enableProdMode

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { authPaths, authRoutes } from './app/modules/auth/shared/auth-routes';
-
+enableProdMode();
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(HttpClientModule),
